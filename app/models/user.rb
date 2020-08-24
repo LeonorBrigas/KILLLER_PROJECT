f6f55571aa7b632ee56757bc14d5c767e6266574
Leonor Brigas <leonor.brigas@gmail.com>
2020-08-24 23:58:26 +0100

    destroyed model user to update to devise user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,6 @@
 class User < ApplicationRecord
-  has_many :reservations
-
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  validates :first_name, :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :encrypted_password, presence: true
-
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
