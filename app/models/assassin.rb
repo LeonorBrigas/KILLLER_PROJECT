67269acd5c0d4e1cc8f42c2d76e6c37a7bfa1ace
Meg Bailey <megxbailey@gmail.com>
2020-08-24 16:50:04 +0100

    this controller is more complete now

--- app/models/assassin.rb
+++ app/models/assassin.rb
@@ -1,3 +1,12 @@
 class Assassin < ApplicationRecord
-    has_many :reservations 
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+#   validates :photo, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
+  has_many :reservations
+#   has_many :reviews, through: :reservations
+#   has_attachment :photo
 end
