fae2e9e6bad68c4ec5fd0a1537b139e0cc584ab8
Leonor Brigas <leonor.brigas@gmail.com>
2020-08-25 16:55:37 +0100

    routes new and more changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-    
+
 10.times do
- user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, 
+ user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email,
  password: Faker::Internet.password)
 end
 
@@ -19,9 +19,9 @@ end
 10.times do
  assassin = Assassin.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, latitude: Faker::Address.latitude,
  longitude: Faker::Address.longitude, total_kills: Faker::Number.within(range: 1..50), has_gun: Faker::Boolean.boolean, has_knife: Faker::Boolean.boolean,
- has_candlestick: Faker::Boolean.boolean, has_wrench: Faker::Boolean.boolean, has_rope: Faker::Boolean.boolean, summary: Faker::Quote.matz, 
+ has_candlestick: Faker::Boolean.boolean, has_wrench: Faker::Boolean.boolean, has_rope: Faker::Boolean.boolean, summary: Faker::Quote.matz,
  price: Faker::Commerce.price, user: User.first)
- 
+
 end
 
 
