5e1e7f69ac2bd6fe3a0f6cbc7dbaf202e53d2a3b
Leonor Brigas <leonor.brigas@gmail.com>
2020-08-25 12:10:10 +0100

    fixing database

--- db/migrate/20200825105999_create_reservations.rb
+++ db/migrate/20200825105999_create_reservations.rb
@@ -1,6 +1,10 @@
 class CreateReservations < ActiveRecord::Migration[6.0]
   def change
     create_table :reservations do |t|
+      t.datetime :start_date
+      t.datetime :end_date
+      t.integer :price
+      t.string :description
       t.references :user, null: false, foreign_key: true
       t.references :assassin, null: false, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_142759) do
+ActiveRecord::Schema.define(version: 2020_08_25_105999) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,27 +18,39 @@ ActiveRecord::Schema.define(version: 2020_08_24_142759) do
   create_table "assassins", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.integer "latitude"
-    t.integer "longitude"
-    t.integer "total_kills"
-    t.boolean "has_gun"
-    t.boolean "has_knife"
-    t.boolean "has_candlestick"
-    t.boolean "has_wrenc"
-    t.boolean "has_rope"
-    t.text "summary"
-    t.integer "price"
+    t.string "photo"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_assassins_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.integer "price"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.bigint "assassin_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["assassin_id"], name: "index_reservations_on_assassin_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "password"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "assassins", "users"
+  add_foreign_key "reservations", "assassins"
+  add_foreign_key "reservations", "users"
 end
